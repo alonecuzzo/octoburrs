9d0ad1d97ab6108fefdac74e2502d058c4c72e1a
Jabari Bell <jabari.bell@pxlflu.net>
2017-10-15 16:17:22 -0400

    ViewModel and service added for profile.

--- octoburrs/AppDelegate.swift
+++ octoburrs/AppDelegate.swift
@@ -18,21 +18,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
-    
+    /// Configuration
     let token = "e5e1263d0febf38385a334250e4afbf2dae51587"
-    let config = TokenConfiguration(token)
+    let service = OctoProfileService(token)
+    let profileViewModel = ProfileViewModel(service: service)
     
-    Octokit(config).me() { response in
-      switch response {
-      case .success(let user):
-        print(user.login)
-      
-      case .failure(let error):
-        print(error)
-      }
-    }
-    
-    let profileViewController = ProfileViewController()
+    let profileViewController = ProfileViewController(viewModel: profileViewModel)
     self.window = UIWindow(frame: UIScreen.main.bounds)
     self.window?.rootViewController = profileViewController
     self.window?.makeKeyAndVisible()
--- octoburrs/ViewController/ProfileViewController.swift
+++ octoburrs/ViewController/ProfileViewController.swift
@@ -8,11 +8,79 @@
 
 import Foundation
 import UIKit
+import RxSwift
+import Octokit
 
+
+protocol ProfileFetchable {
+  func fetchProfile() -> Observable<User>
+}
+
+struct OctoProfileService: ProfileFetchable {
+  
+  private let config: TokenConfiguration
+  
+  init(_ token: String) {
+    self.config = TokenConfiguration(token)
+  }
+  
+  func fetchProfile() -> Observable<User> {
+    return Observable.create { observer -> Disposable in
+      Octokit(self.config).me() { response in
+        switch response {
+        case .success(let user):
+          observer.on(.next(user))
+          //observer.on(.completed)
+        case .failure(let error):
+          observer.onError(error)
+        }
+      }
+     return Disposables.create()
+    }
+  }
+}
+
+struct ProfileViewModel {
+  
+
+  private let service: ProfileFetchable
+  let user = Variable(User(["": "" as AnyObject]))
+  private let disposeBag = DisposeBag()
+  
+  
+  init(service: ProfileFetchable) {
+    self.service = service
+  }
+  
+  
+  func fetchUser() {
+    service.fetchProfile().subscribe(onNext: { user in
+      self.user.value = user
+    }).disposed(by: disposeBag)
+  }
+}
 
 class ProfileViewController: UIViewController {
+  
+  private let viewModel: ProfileViewModel
+  private let disposeBag = DisposeBag()
+  
+  required init(viewModel: ProfileViewModel) {
+    self.viewModel = viewModel
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .purple
+    viewModel.fetchUser()
+    //get the user object
+    viewModel.user.asObservable().skip(1).subscribe(onNext: { user in
+      print(user.login)
+    }).disposed(by: disposeBag)
   }
 }
