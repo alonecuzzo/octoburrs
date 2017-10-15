41578c44e2e9a4aea97fdfc169312d24193524c4
Jabari Bell <jabari.bell@pxlflu.net>
2017-10-15 15:08:05 -0400

    Added Profileviewcontroller.

---
+++ octoburrs/ViewController/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  octoburrs
+//
+//  Created by Jabari Bell on 10/15/17.
+//  Copyright © 2017 theowl. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class ProfileViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .purple
+  }
+}
